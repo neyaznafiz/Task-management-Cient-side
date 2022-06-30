892ec35615da974d58799ebf8b68555cf86a2d12
niiyazobug <niiyaz5555@gmail.com>
2022-06-30 19:21:51 +0600

    calender date style css added

--- src/Pages/Calender.js
+++ src/Pages/Calender.js
@@ -4,23 +4,48 @@ import 'react-day-picker/dist/style.css';
 import { useState } from 'react'
 import { format } from 'date-fns';
 
+const css = `
+        .my-selected:not([disabled]) { 
+            font-weight: bold;
+            color: #05a962; 
+            border: 2px solid #05a962;
+          }
+          .my-selected:hover:not([disabled]) { 
+            border-color: #05a962;
+            color: #05a962;
+          }
+          .my-today {
+              padding: 20px;
+            font-size: 130%; 
+            color: #05a962;
+          }
+    `
+
 function Calender() {
 
-const [date, setDate]= useState(new Date())
+    const [date, setDate] = useState(new Date())
+
+
+
     return (
         <div>
             <div className=" ">
                 <div className="">
                     <div className=''>
-                         <DayPicker
+                        <style>{css}</style>
+                        <DayPicker
                             mode="single"
                             selected={date}
                             onSelect={setDate}
-                            // footer={footer}
+                            modifiersClassNames={{
+                                selected: 'my-selected',
+                                today: 'my-today'
+                            }}
+                        // footer={footer}
                         />
                         <p>You have selected: {format(date, 'PPPP')}.</p>
                     </div>
-                </div> 
+                </div>
             </div>
         </div>
     )

c47b9f337708118f2651ca0f143ee3eefaa743f6
niiyazobug <niiyaz5555@gmail.com>
2022-06-30 15:54:07 +0600

    loading component created

---
+++ src/Components/Shared/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { DotLoader } from 'react-spinners'
+
+function Loading() {
+    return (
+        <DotLoader color='#05a962' size={50} />
+    )
+}
+
+export default Loading

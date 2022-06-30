5847a17bce240215bb01b07b12dddbe5782529af
niiyazobug <niiyaz5555@gmail.com>
2022-06-30 15:08:20 +0600

    navbar added dark lite moog added and component created

---
+++ src/Pages/CompletedTasks.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CompletedTasks() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CompletedTasks
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+
+  daisyui: {
+    themes: [
+      "dark",
+      "light",
+    ],
   },
-  plugins: [],
+
+  plugins: [
+    require("daisyui"),
+  ],
+  
 }
